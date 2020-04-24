cc22844a6eb8384282efd57b9fa27d3f6c77977a
Lindsey Morrison <limorris@microsoft.com>
2020-04-24 12:19:32 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hackathon_backend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
